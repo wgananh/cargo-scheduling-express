7f6fcd13c5429dbf62947111810f9fe842364206
wanghangong <17531865118@163.com>
2024-01-12 23:12:25 +0800

    添加websocket

--- index.js
+++ index.js
@@ -157,6 +157,34 @@ app.post("/api/check", async (req, res) => {
 });
 
 
+app.checkStatus('/checkStatus', function (ws, req) {
+  let openid = req.headers['x-wx-openid'] // 从header中获取用户openid信息
+  if (openid == null) { // 如果不存在则不是微信侧发起的
+    openid = new Date().getTime() // 使用时间戳代替
+  }
+  if (connect[openid] != null) { // 判断用户是否有连接
+    ws.send('当前用户已经在其他设备连接过，无法重复连接') // 发送重复连接信息
+    ws.close() // 关闭连接
+  } else {
+    connect[openid] = { // 记录用户信息
+      openid: openid, // 用户openid
+      source: req.headers['x-wx-source'] || '非微信', // 用户微信来源
+      unionid: req.headers['x-wx-unionid'] || '-', // 用户unionid
+      ip: req.headers['x-forwarded-for'] || '未知' // 用户所在ip地址
+    }
+    console.log('链接请求头信息', req.headers)
+    ws.on('message', function (msg) {
+      console.log('收到消息：', msg)
+      ws.send(`收到-${msg}`)
+    })
+    ws.on('close', function () {
+      console.log('链接断开：', openid)
+      delete connect[openid]
+    })
+  }
+})
+
+
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
